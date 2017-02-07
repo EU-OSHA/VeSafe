808d1acfa9708548db3b57b2865546e4d98ca726
Cristian Romanescu <cristian@romanescu.ro>
2017-02-07 13:18:04 +0200

    Aliases

--- drush/aliases/osha.aliases.drushrc.php
+++ drush/aliases/osha.aliases.drushrc.php
@@ -1,32 +1,24 @@
 <?php
 
-$aliases['edw-staging'] = array(
-  'uri' => 'osha.edw.ro',
+// This alias is used in install and update scripts.
+// Rewrite it in your aliases.local.php as you need.
+
+$aliases['staging'] = array(
+  'uri' => 'http://vesafe.edw.ro',
   'db-allows-remote' => TRUE,
-  'remote-host' => 'php-osha.edw.ro',
+  'remote-host' => '5.9.54.24',
   'remote-user' => 'php',
-  'root' => '/var/www/html/docroot',
+  'root' => '/var/www/html/osha-vesafe/docroot',
   'path-aliases' => array(
     '%files' => 'sites/default/files',
   ),
   'command-specific' => array(
     'sql-sync' => array(
       'simulate' => '1',
-      'source-dump' => '/tmp/source-oira-dump.sql',
+      'source-dump' => '/tmp/osha-vesafe-dump-cristiroma.sql',
     ),
   ),
 );
-
-$aliases['staging'] = $aliases['edw-staging'];
-
-// This alias is used in install and update scripts.
-// Rewrite it in your aliases.local.php as you need.
-
-// Example of local setting.
-// $aliases['osha.local'] = array(
-//   'uri' => 'osha.localhost',
-//   'root' => '/home/my_user/osha-website/docroot',
-// );
 
 // Add your local aliases.
 if (file_exists(dirname(__FILE__) . '/aliases.local.php')) {
