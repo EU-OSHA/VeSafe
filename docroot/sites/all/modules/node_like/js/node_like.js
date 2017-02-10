837f298509e288e551b9313434e7f548c7bbf118
Cristian Romanescu <cristian@romanescu.ro>
2017-02-11 00:45:54 +0200

    Integrated the like functionality

---
+++ docroot/sites/all/modules/node_like/js/node_like.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function () {
+  jQuery('a.node-like-link').click(function () {
+    var link = this;
+    jQuery.ajax({
+      type: 'POST',
+      url: this.href,
+      dataType: 'json',
+      success: function (data) {
+        if (typeof data.selector == 'string') {
+          jQuery(link).attr('title', 'Thank you');
+          jQuery(link).attr('data-toggle', 'tooltip');
+          jQuery(link).tooltip('show');
+          jQuery(data.selector).text(data.text);
+        }
+      },
+      data: 'js=1'
+    });
+    return false;
+  });
+});
