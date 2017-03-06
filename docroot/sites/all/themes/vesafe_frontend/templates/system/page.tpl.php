e250adc9b23324b0451e4f2ee5d09d526764f672
Endika Llonin <ellonin@bilbomatica.es>
2017-03-06 16:41:37 +0100

    css changes - fixing header responsive issu

--- docroot/sites/all/themes/vesafe_frontend/templates/system/page.tpl.php
+++ docroot/sites/all/themes/vesafe_frontend/templates/system/page.tpl.php
@@ -175,7 +175,7 @@
 
 <div class="page-content-container container">
   <?php if (!empty($page['sidebar_first'])): ?>
-    <aside class="col-sm-3" role="complementary">
+    <aside class="col-md-3" role="complementary">
       <?php print render($page['sidebar_first']); ?>
     </aside>  <!-- /#sidebar-first -->
   <?php endif; ?>
