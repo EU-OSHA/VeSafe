0f61d4647beab47454bfd21a00210e67ccd34e8a
BILBOMATICA2\dprado <dprado@bilbomatica.es>
2017-03-24 11:18:06 +0100

    VES-96

--- docroot/sites/all/modules/node_like/js/node_like.js
+++ docroot/sites/all/modules/node_like/js/node_like.js
@@ -1,4 +1,24 @@
 jQuery(document).ready(function () {
+  var likedNodes = jQuery.cookie('liked-nodes');
+  if (likedNodes != null && likedNodes != ''){
+    likedNodes = likedNodes.split(',');
+    var links = jQuery('a.node-like-link');
+    var href = '';
+    for (var i = 0; i < links.size(); i++) {
+      href = jQuery(links[i]).attr('href');
+      href = href.replace('/node/','');
+      href = href.replace('/like', '');
+
+      if (likedNodes.indexOf(href) > -1) {
+        jQuery(links[i]).attr('title', 'You have already liked this node');
+        jQuery(links[i]).attr('onclick', 'return false');
+        jQuery(links[i]).attr('href', '#');
+        jQuery(links[i]).addClass('node-liked-link');
+        jQuery(links[i]).removeClass('node-like-link');
+      }
+    }
+  }
+
   jQuery('a.node-like-link').click(function () {
     var link = this;
     jQuery.ajax({
@@ -11,10 +31,20 @@ jQuery(document).ready(function () {
           jQuery(link).attr('data-toggle', 'tooltip');
           jQuery(link).tooltip('show');
           jQuery(data.selector).text(data.text);
+          var text = data.selector;
+          text = text.replace('#node-like-','');
+          if (jQuery.cookie('liked-nodes') != null) {
+            jQuery.cookie('liked-nodes', jQuery.cookie('liked-nodes') + ',' + text);
+          }else{
+            jQuery.cookie('liked-nodes', text);
+          }
         }
       },
       data: 'js=1'
     });
+    jQuery(this).addClass('node-liked-link');
+    jQuery(this).removeClass('node-like-link');
+
     return false;
   });
 });
