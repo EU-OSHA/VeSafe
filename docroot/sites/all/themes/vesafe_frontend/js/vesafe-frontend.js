23af32a823a51621b7410961396267fa2c0bbcd2
vadimski <vadeemk@gmail.com>
2017-04-11 12:23:47 +0300

    VES-100 - HTML Validation - 12.Contact

--- docroot/sites/all/themes/vesafe_frontend/js/vesafe-frontend.js
+++ docroot/sites/all/themes/vesafe_frontend/js/vesafe-frontend.js
@@ -7,6 +7,7 @@ jQuery(document).ready(function () {
 	// 	wrap: 'word',
 	// });
 
+	jQuery("[for=security_code]").remove();
 	jQuery("[for=edit-captcha-response]").attr("for","security_code");
 
     jQuery(".changer").addClass("notranslate");
