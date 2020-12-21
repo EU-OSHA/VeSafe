8a0f9df08900b31539d773cc506aaeb75c532cd6
Rocio Vera Camacho <ruben.ramos@bilbomatica.es>
2020-12-21 11:58:00 +0100

    Deleted piwik configuration code

--- docroot/sites/all/themes/vesafe_frontend/templates/system/html.tpl.php
+++ docroot/sites/all/themes/vesafe_frontend/templates/system/html.tpl.php
@@ -62,17 +62,6 @@
   a.appendChild(r);
 })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
-<script type="text/javascript">var _paq = _paq || [];
-(function(){var u=(("https:" == document.location.protocol) ? "https://piwik.osha.europa.eu/piwik/" : "http://piwik.osha.europa.eu/piwik/");
-_paq.push(["setSiteId", "24"]);
-_paq.push(["setTrackerUrl", u+"piwik.php"]);
-_paq.push(["setDoNotTrack", 1]);
-_paq.push(["trackPageView"]);
-_paq.push(["setIgnoreClasses", ["no-tracking","colorbox"]]);
-_paq.push(["enableLinkTracking"]);
-var d=document,g=d.createElement("script"),s=d.getElementsByTagName("script")[0];
-g.type="text/javascript";g.defer=true;g.async=true;g.src=u+"piwik.js";s.parentNode.insertBefore(g,s);})();
-</script>
 
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
